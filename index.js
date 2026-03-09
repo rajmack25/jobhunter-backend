f3bdf093d80ea41055aece14249d070fc80d92d6
rajmack25 <jaraztimothy@gmail.com>
2026-03-09 21:03:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -77,4 +77,8 @@ app.get('/jobs', (req, res) => {
 });
 
 client.initialize();
-app.listen(process.env.PORT || 3001);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
